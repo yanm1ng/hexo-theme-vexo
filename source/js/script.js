cd43d8ac99213ea26e41f064fecc7a1dea61d2e3
yanm1ng <yummy1121@foxmail.com>
2017-06-06 23:45:19 +0800

    added scroll-top component, and finnsh post image banner

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,44 @@
 (function($){
+
+  function smoothScroll() {
+    var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+    if (currentScroll > 0) {
+      window.requestAnimationFrame(smoothScroll);
+      window.scrollTo(0, currentScroll - (currentScroll / 5));
+    }
+  }
+
   var header = $('.header')
+  var banner = document.getElementById('article-banner') || false
+  var top = $('.scroll-top')
 
   $('.menu').on('click', function() {
     $('.menu-mask').toggleClass('open')
   })
 
-  if(!header.data('ispost') || header.data('ishome')){
-    header.addClass('fixed-header');
+  top.on('click', function() {
+    smoothScroll()
+  })
+
+  if (!header.data('ispost') || header.data('ishome')) {
+    header.addClass('fixed-header')
   }
+
+  document.addEventListener('scroll', function() {
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    var headerH = header.height()
+    if (banner) {
+      if (scrollTop > headerH) {
+        header.addClass('fixed-header')
+      } else {
+        header.removeClass('fixed-header')
+      }
+    }
+    if (scrollTop > 100) {
+      top.addClass('opacity')
+    } else {
+      top.removeClass('opacity')
+    }
+  });
 
 })(jQuery);
