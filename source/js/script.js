b36e90c6d2844d8cd20f4f40ae880680840cec7e
yanm1ng <yummy1121@foxmail.com>
2017-06-06 09:32:18 +0800

    added post banner image

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,12 @@
 (function($){
+  var header = $('.header')
+
   $('.menu').on('click', function() {
     $('.menu-mask').toggleClass('open')
   })
+
+  if(!header.data('ispost') || header.data('ishome')){
+    header.addClass('fixed-header');
+  }
+
 })(jQuery);
