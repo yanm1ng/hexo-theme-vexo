8ab90b991d77e6456ced5700a174634784d14cae
yanm1ng <yummy1121@foxmail.com>
2017-07-06 22:51:24 +0800

    ready for code style test

---
+++ lint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Lint EJS files
+for f in $(find ./layout -name "*.ejs"); do
+  echo $f;
+  node_modules/.bin/ejslint $f;
+done
+
+# Lint JS files
+for f in $(find ./source/js -name "*.js"); do
+  echo $f;
+  node_modules/.bin/eslint $f;
+done
